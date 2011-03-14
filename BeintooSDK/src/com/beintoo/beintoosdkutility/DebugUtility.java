63ab90c2c09628f03b5e78fce033eeb784f53261
smoi <smoi@beintoo.com>
2011-03-14 15:04:19 +0100

    BeintooSDK v1.1 beta

--- BeintooSDK/src/com/beintoo/beintoosdkutility/DebugUtility.java
+++ BeintooSDK/src/com/beintoo/beintoosdkutility/DebugUtility.java
@@ -3,7 +3,7 @@ package com.beintoo.beintoosdkutility;
 import android.util.Log;
 
 public class DebugUtility {
-	static boolean isDebugEnable = true;
+	static boolean isDebugEnable = false;
 	
 	public static void showLog (String output){
 		if(isDebugEnable)
